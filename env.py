f5c7683e251741a4a929f50a90192a008311d99a
Jacob Clermont <1748662@cstjean.qc.ca>
2020-10-30 19:24:57 -0400

    FIN env.py

---
+++ env.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Programme pour accéder aux variables d'environnement
+
+Par Jacob Clermont
+"""
+import getpass
+import os
+# import pprint
+
+
+def main() -> None:
+    """Fonction principale"""
+    print(f"Il y a {len(os.environ)} variables d'environnement")
+    print(f" - OS: {os.getenv('OS', 'Non défini')}")
+    print(f" - HOME: {os.getenv('HOMEPATH', os.getenv('HOME', 'Non défini'))}")
+
+    print()
+    print("Code portable:")
+    print(f" - {os.name=}")
+    print(f" - user: {getpass.getuser()}")
+    print(f" - home: {os.path.expanduser('~')}")
+
+
+if __name__ == '__main__':
+    main()
