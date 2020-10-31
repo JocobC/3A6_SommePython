0325d4348fdfc678e3c414c5125f6fda3184da42
Jacob Clermont <1748662@cstjean.qc.ca>
2020-10-31 15:32:29 -0400

    FIN now.py

---
+++ now.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Programme qui affiche des dates
+
+Par Jacob Clermont
+"""
+import datetime
+
+
+def main() -> None:
+    """Fonction principale"""
+    maintenant = datetime.datetime.today()
+    noel = datetime.date(2020, 12, 25)
+    avantNoel = datetime.date(2020, 12, 25) - maintenant.date()
+    print(f"maintenant: {maintenant}")
+    print(f"aujourd'hui: {maintenant.date()}")
+    print(f"demain: {maintenant.date() + datetime.timedelta(days=1)}")
+    print(f"avant-hier: {maintenant.date() + datetime.timedelta(days=-2)}")
+    print(f"noel: {noel}")
+    print(f"noel dans: {avantNoel.days} jours")
+
+
+if __name__ == '__main__':
+    main()
